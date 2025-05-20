ea33b5e045aab7b1a2f9c228a0dc52eef0aba72d
maxxxxyang <lilun_12345@126.com>
2025-05-20 17:42:51 +0800

    fix: 视频稿件管理接口去除 video 前缀，前后端路径规范化

--- kinit-api/apps/vadmin/video/views.py
+++ kinit-api/apps/vadmin/video/views.py
@@ -22,29 +22,29 @@ app = APIRouter()
 ###########################################################
 #    视频稿件管理
 ###########################################################
-@app.get("/articles", summary="获取视频稿件列表", tags=["视频稿件管理"])
+@app.get("/article", summary="获取视频稿件列表", tags=["视频稿件管理"])
 async def get_video_article_fw_list(p: params.VideoArticleFwParams = Depends(), auth: Auth = Depends(AllUserAuth())):
     datas, count = await crud.VideoArticleFwDal(auth.db).get_datas(**p.dict(), v_return_count=True)
     return SuccessResponse(datas, count=count)
 
 
-@app.post("/articles", summary="创建视频稿件", tags=["视频稿件管理"])
+@app.post("/article", summary="创建视频稿件", tags=["视频稿件管理"])
 async def create_video_article_fw(data: schemas.VideoArticleFw, auth: Auth = Depends(AllUserAuth())):
     return SuccessResponse(await crud.VideoArticleFwDal(auth.db).create_data(data=data))
 
 
-@app.delete("/articles", summary="删除视频稿件", description="硬删除", tags=["视频稿件管理"])
+@app.delete("/article", summary="删除视频稿件", description="硬删除", tags=["视频稿件管理"])
 async def delete_video_article_fw_list(ids: IdList = Depends(), auth: Auth = Depends(AllUserAuth())):
     await crud.VideoArticleFwDal(auth.db).delete_datas(ids=ids.ids, v_soft=False)
     return SuccessResponse("删除成功")
 
 
-@app.put("/articles/{data_id}", summary="更新视频稿件", tags=["视频稿件管理"])
+@app.put("/article/{data_id}", summary="更新视频稿件", tags=["视频稿件管理"])
 async def put_video_article_fw(data_id: int, data: schemas.VideoArticleFw, auth: Auth = Depends(AllUserAuth())):
     return SuccessResponse(await crud.VideoArticleFwDal(auth.db).put_data(data_id, data))
 
 
-@app.get("/articles/{data_id}", summary="获取视频稿件信息", tags=["视频稿件管理"])
+@app.get("/article/{data_id}", summary="获取视频稿件信息", tags=["视频稿件管理"])
 async def get_video_article_fw(data_id: int, db: AsyncSession = Depends(db_getter)):
     schema = schemas.VideoArticleFwSimpleOut
     return SuccessResponse(await crud.VideoArticleFwDal(db).get_data(data_id, v_schema=schema))
